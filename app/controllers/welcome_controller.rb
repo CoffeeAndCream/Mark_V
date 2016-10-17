c1902382aa9c4cbd3ed0c19153195bfe3af045f8
CoffeeAndCream <seanjhulse@gmail.com>
2016-10-17 09:31:44 -0500

    Javascript testing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,7 +9,7 @@ class WelcomeController < ApplicationController
   	@chart = Fusioncharts::Chart.new({
       	:height => 400,
       	:width => 600,
-      	:type => 'mscolumn2d',
+      	:type => 'line2d',
       	:renderAt => 'chart-container',
 
   # Chart data is passed to the `dataSource` parameter, as hashes, in the form of
