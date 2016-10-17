aa4a08f3ced37b04a0c451575f63911e885be80b
CoffeeAndCream <seanjhulse@gmail.com>
2016-10-17 10:48:43 -0500

    Create a function that converts ruby objects to JSON

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,55 @@ class WelcomeController < ApplicationController
 
   # JSON and the chart will be rendered.
   def index
-    @user = "Test"
-    var subCaption = @user.to_json;
+    def output
+      @x = rand(10...10000)
+    end
+    @test = "Did you drink that coffee?"
+  # Create the FusionCharts object in the controller action
+
+  	@chart = Fusioncharts::Chart.new({
+      	:height => 400,
+      	:width => 600,
+      	:type => 'line',
+      	:renderAt => 'chart-container2',
+
+  # Chart data is passed to the `dataSource` parameter, as hashes, in the form of
+
+  # key-value pairs.
+
+  :dataSource => {
+    	:chart => {
+        	:caption => @test,
+        	:subCaption => 'Coffee Index',
+        	:xAxisname => 'Quarter',
+        	:yAxisName => 'Amount ($)',
+        	:numberPrefix => '$',
+        	:theme => 'fint',
+    	},
+
+  # The `category` hash is defined inside the `categories` array with four key-value pairs
+
+  # that represent the x-axis labels for the four quarters.
+
+    	:categories => [{
+        	:category => [
+            	{ :label => 'Q1' },
+            	{ :label => 'Q2' },
+            	{ :label => 'Q3' },
+            	{ :label => 'Q4' }
+        	]
+    	}],
+    	:dataset =>  [{
+  # The `data` hash contains four key-value pairs that are the values for the revenue
+  # generated in the previous year.
+        	:data =>  [
+            	{ :value => @x },
+            	{ :value => '11500' },
+            	{ :value => '12500' },
+            	{ :value => '15000' }
+        	]}
+    	]
+    }
+  })
   end
 end
