733fd0c31780882975210bd243568f2d1d17724e
CoffeeAndCream <seanjhulse@gmail.com>
2016-10-17 17:10:19 -0500

    China Imported Goods added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,11 +4,19 @@ class WelcomeController < ApplicationController
   # The `fc_hash` function is defined to load data from a Ruby Hash. This data will be converted to
   # JSON and the chart will be rendered.
   def index
-    @import_data = create_hash('public/datasets/IMPJP.csv')
-    graph_arr = []
+    @import_jp_data = create_hash('public/datasets/IMPJP.csv')
+    @import_ch_data = create_hash('public/datasets/IMPCH.csv')
+    jp_arr = []
+    ch_arr = []
 
-    @import_data.each do |key, val|
-      graph_arr.push({
+    @import_jp_data.each do |key, val|
+      jp_arr.push({
+          :label => key,
+          :value => val
+        })
+        end
+    @import_ch_data.each do |key, val|
+      ch_arr.push({
           :label => key,
           :value => val
         })
@@ -29,12 +37,32 @@ class WelcomeController < ApplicationController
             	:yAxisName => 'Amount of Goods ($ millions)',
             	:numberPrefix => '$',
               :showValues => '0',
-              :labelStep => 3,
+              :labelStep => '3',
             	:theme => 'fint',
         	},
-          :data => graph_arr
+          :data => jp_arr
         }
       })
+      @chchart = Fusioncharts::Chart.new({
+      	:height => 600,
+      	:width => 800,
+      	:type => 'line',
+      	:renderAt => 'chchart-container',
+
+        :dataSource => {
+          	:chart => {
+              	:caption => 'Imports from Japan to the USA',
+              	:subCaption => "Imported Goods",
+              	:xAxisname => 'Date Recorded',
+              	:yAxisName => 'Amount of Goods ($ millions)',
+              	:numberPrefix => '$',
+                :showValues => '0',
+                :labelStep => '3',
+              	:theme => 'carbon',
+          	},
+            :data => ch_arr
+          }
+        })
   end
 
   def create_hash(filepath)
