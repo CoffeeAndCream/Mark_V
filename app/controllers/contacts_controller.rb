55a4fcf01958a37c47571cdfc06313a01201d262
CoffeeAndCream <seanjhulse@gmail.com>
2016-10-18 21:39:13 -0500

    Mailing with mailgun?

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,34 @@
 class ContactsController < ApplicationController
-  def new
-    @contact = Contact.new
+  def index
   end
 
   def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    # Get message value from form
+    @message = params
+    # Check if SMTP
+    if @message[:mode] == "SMTP"
+      # Tell notifier action mailer to send via SMTP
+      Notifier.send_email(@message).deliver
+      # Redirect on success
+      redirect_to root_path, notice: 'Message was sent via SMTP.'
+    # Else send via API
     else
-      flash.now[:error] = 'Cannot send message.'
-      render :new
+      # First, instantiate the Mailgun Client with your API key
+      mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
+
+      # Define your message parameters
+      html_output = render_to_string template: "notifier/send_email"
+      message_params = {:from    => 'Mailgun Test Project <postmaster@' + ENV['MAILGUN_DOMAIN'] + ">",
+                        :to      => @message[:to],
+                        :subject => 'Mailgun message via API',
+                        :text    => @message[:body],
+                        :html    => html_output.to_str,
+                        "o:tag"  => 'test'}
+
+      # Send your message through the client
+      mg_client.send_message ENV['MAILGUN_DOMAIN'], message_params
+      # Redirect on success
+      redirect_to root_path, notice: 'Message was sent via API.'
     end
   end
 end
